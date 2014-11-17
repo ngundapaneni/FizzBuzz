02b4e6b5fa6e7523dbce61bf8fa9ffbd0827fa0a
Natasha <ngundapaneni@gmail.com>
2014-11-17 06:10:26 -0600

    Fizzbuzz refactor
    Refactor FizzBuzz App

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,7 +1,8 @@
 var fizzBuzzVal = "";
 
-$(document).ready(function() {
-	for(var i=1; i<=100; i++) {
+var fizzBuzzGame = function(number) {
+
+	for(var i=1; i<=number; i++) {
 
 		if(((i%3) == 0) && ((i%5) == 0)) {
 			fizzBuzzVal += "<span style='color:red'>FIZZ BUZZ!</span><br/>";
@@ -16,5 +17,25 @@ $(document).ready(function() {
 			fizzBuzzVal += i + "<br/>";
 		}
 	}
-	$("#fizzBuzz").append(fizzBuzzVal);
+	$("#fizzBuzz").append(fizzBuzzVal);	
+};
+
+var checkIfNumber = function(value) {
+	var isNumber = true;
+	if(value%1 != 0)
+	{
+		isNumber = false;
+ 	}
+ 	else if (value == "") {
+ 		isNumber = false;
+ 	}
+ 	return isNumber;
+};
+
+$(document).ready(function() {
+	var num = +prompt("Enter the number: ");
+	while(!checkIfNumber(num)){
+		num = +prompt("Please enter a valid number: ");
+	}
+	fizzBuzzGame(num);
 });
